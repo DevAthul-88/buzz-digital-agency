e643d20e5c9ca1ab359080439ff10923c557bec9
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2021-08-24 21:03:56 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+
+
+
+
+
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+app.get('/' , function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/pages' , 'home.html'))
+})
+
+app.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/pages' , 'about.html'))
+})
+
+app.get('/contact', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/pages' , 'contact.html'))
+})
+
+app.get('/pricing' , function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/pages' , 'pricing.html'))
+})
+
+app.get('/services/website' , function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/pages' , 'website.html'))
+})
+
+app.get('/services/mobile' , function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/pages' , 'mobile.html'))
+})
+
+
+app.get('/services/market' , function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/pages' , 'market.html'))
+})
+
+app.get('/services/video' , function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/pages' , 'video.html'))
+})
+
+app.get('/services/' , function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/pages' , 'services.html'))
+})
+
+
+const port =  3001
+
+app.listen(port)
